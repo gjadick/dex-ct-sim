a4384d11632e494abfd916e7f3ea6a6728fc6f72
gjadick <giavanna@uchicago.edu>
2023-10-27 16:11:36 -0500

    revised to use xtomosim

--- main.py
+++ main.py
@@ -13,99 +13,167 @@ Script for:
 
 """
 
+import sys
+sys.path.append('xtomosim')  # for xtomosim
 
-import matplotlib.pyplot as plt
-from system import Spectrum, ScannerGeometry, Phantom
-from helpers import get_out_dir_se, get_sino, get_recon, get_matdecomp
+from xtomosim.system import read_parameter_file, xRaySpectrum
+from xtomosim.forward_project import get_sino
+from xtomosim.back_project import get_recon
+from matdecomp import get_basismat_sinos
+
+import os
+import shutil
 from time import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-####################################################################
-### START OF INPUTS
+plt.rcParams.update({
+    'figure.dpi': 300,
+    'font.size':10,
+    'font.family': 'serif',
+    'font.serif': ['Computer Modern Roman'],
+    'text.usetex': True,
+    'axes.titlesize':10,
+    'axes.labelsize':8,
+    'axes.linewidth': .5,
+    'xtick.top': True, 
+    'ytick.right': True, 
+    'xtick.direction': 'in', 
+    'ytick.direction': 'in',
+    'xtick.labelsize':8,
+    'ytick.labelsize':8,
+    'axes.grid' : False, 
+    'grid.color': 'lightgray',
+    'grid.linestyle': ':',
+    'legend.fontsize':8,
+    'lines.linewidth':1,
+    'image.cmap':'gray',
+    'image.aspect':'auto'
+    })
 
-# recon parameters
-N_matrix = 512  # num pixels in recon matrix
-FOV = 50.0      # cm
-ramp = 0.8      # cutoff % of Nyquist frequency
 
-# geometry 
-ct = ScannerGeometry(N_channels=800, N_proj=1200, gamma_fan=0.8230337,   # 47 deg
-                     SID=60.0, SDD=100.0,  pxshape='rect', eid=True, 
-                     detector_file='input/detector/eta_eid_mv.bin')  
+def ax_imshow(fig, ax, img, colorbar=True, title='', kw={}):
+    """Convenience function for showing images with colorbar."""
+    ax.set_title(title)
+    m = ax.imshow(img, **kw)
+    if colorbar:
+        fig.colorbar(m, ax=ax)
+        
+        
+def load_spectrum(spec_id, dose):
+    """Convenience function for loading DECT spectra and scaling dose."""
+    fname = f'./input/spectrum/{spec_id}_1mGy_float32.bin'
+    spec = xRaySpectrum(fname, spec_id)
+    spec.rescale_counts(ct.A_iso * dose / ct.N_proj)  
+    return spec
 
-# phantom 
-id_phantom = 'cylinder_5mats'
-filename_phantom = 'input/phantom/cylinder_5mats_512x512_uint8.bin'
-filename_matcomp = 'input/phantom/cylinder_5mats_matcomp.txt'
-phantom = Phantom(id_phantom, filename_phantom, filename_matcomp, Nx=512, Ny=512, Nz=1, ind=0) 
- 
-# spectra -- can choose any dual energy combo from these options
-filepath_spectrum = 'input/spectrum/'
-filename_dict = {
-    '6MV':       "Accuray_treatment6MV.csv", 
-    'detunedMV': "Accuray_detuned.csv", 
-    '80kV':      "spec80.mat", 
-    '120kV':     "spec120.mat", 
-    '140kV':     "spec140.mat"     }
 
-### END OF INPUTS    
-####################################################################
 
 
 if __name__ == '__main__':
-    """
-    Main loop over all the dual energy spectral combinations.
-    Does forward projection, reconstruction, and material decomposition.
-    ---
-    spec_id1, spec_id2 : 
-        spectrum names (see filename_dict above)
-    D1, D2 :
-        corresponding dose in mGy
-    """
-    for spec_id1, spec_id2, D1, D2 in [['140kV', '80kV', 5, 5]]:
+    
+    
+    ###########################################################################
+    ### INPUTS
+    
+    param_file = './input/params.txt'  # only requires geometry and phantom
+    main_output_dir = './output/'
+    show_imgs = True
+    
+    ### END OF INPUTS
+    ###########################################################################
+    
+    
+    # Unpack scanner geometry and phantom parameters.
+    params = read_parameter_file(param_file)[0]
+    run_id, do_forward_projection, do_back_projection = params[:3]
+    ct, phantom, _ = params[3:6]  # ignore spectrum, assign in DECT main loop below
+    if do_back_projection:
+        N_matrix, FOV, ramp = params[6:9]
+            
+    out_dir = os.path.join(main_output_dir, run_id)
+    os.makedirs(out_dir, exist_ok=True)
+    shutil.copy(param_file, os.path.join(out_dir, 'params.txt'))
+    
+    # Main loop over all the dual energy spectral combinations.
+    for spec_id1, spec_id2, D1, D2 in [['detunedMV', '80kV', 9, 1],
+                                       ['140kV',     '80kV', 5, 5]]:
         t0 = time()
-
-        ## 1 : Single energy acquisitions with each spectrum
+        dect_specs = []
+        dect_sinos = []
+        
+        # 1 : SINGLE ENERGY
         for spec_id, dose in [[spec_id1, D1], [spec_id2, D2]]:
-     
-            # Load spectrum with dose initialized at 1 mGy before scaling.
-            spec = Spectrum(filepath_spectrum+f'{spec_id}_1mGy_float32.bin', spec_id)
-            spec.rescale_I0(ct.A_iso * dose / ct.N_proj)  # rescale by pixel area / number of views
-                
-            out_dir = get_out_dir_se(ct, phantom, spec, dose)
-            print(f'\n{out_dir}')
 
-            ### 1a : forward project
-            sino_raw, sino_log = get_sino(out_dir, ct, phantom, spec)    
-            fig, ax = plt.subplots(1, 2, dpi=300, figsize=[9,5])
-            m = ax[0].imshow(sino_raw, cmap='gray')
-            fig.colorbar(m, ax=ax[0])
-            m = ax[1].imshow(sino_log, cmap='gray')
-            fig.colorbar(m, ax=ax[1])
+            sub_dir = os.path.join(out_dir, f'{spec_id}_{int(dose*1000):04}uGy/') 
+            os.makedirs(sub_dir, exist_ok=True)
+            print(f'\n*** {sub_dir} ***')
+            
+            spec = load_spectrum(spec_id, dose)
+            dect_specs.append(spec)
+            
+            # 1a : Forward project for one spectrum.
+            print('Forward projecting!')
+            sino_raw, sino_log = get_sino(ct, phantom, spec)   
+            sino_raw.astype(np.float32).tofile(sub_dir+'sino_raw_float32.bin')
+            sino_log.astype(np.float32).tofile(sub_dir+'sino_log_float32.bin')
+            if show_imgs:
+                fig, ax = plt.subplots(1, 2, figsize=[7,3])
+                ax_imshow(fig, ax[0], sino_raw, title='Raw line integrals')
+                ax_imshow(fig, ax[1], sino_log, title='Log sinogram')
+                fig.tight_layout()
+                plt.show()
+            dect_sinos.append(sino_raw)
+
+            # 1b : Reconstruct the one image.
+            if do_back_projection:
+                print('Back projecting!')
+                recon_raw, recon_HU = get_recon(sino_log, ct, spec, N_matrix, FOV, ramp) 
+                recon_raw.astype(np.float32).tofile(sub_dir+'recon_raw_float32.bin')
+                recon_HU.astype(np.float32).tofile(sub_dir+'recon_HU_float32.bin')
+                if show_imgs:
+                    fig, ax = plt.subplots(1, 2, figsize=[7,3])
+                    ax_imshow(fig, ax[0], recon_raw, title='Raw reconstruction [cm$^{-1}$]')
+                    ax_imshow(fig, ax[1], recon_HU, title='Hounsfield Units')
+                    fig.tight_layout()
+                    plt.show()
+
+        # 2 : DUAL ENERGY
+        spec1, spec2 = dect_specs
+        sino1, sino2 = dect_sinos
+        sub_dir = os.path.join(out_dir, f'matdecomp_{spec_id1}_{spec_id2}_{int(D1*1000):04}uGy_{int(D2*1000):04}uGy/') 
+        os.makedirs(sub_dir, exist_ok=True)
+        print(f'\n*** {sub_dir} ***')
+
+        # 2a : Apply Gauss-Newton material decomposition to get basis material sinograms.
+        print('Decomposing into basis material sinograms!')
+        matsino1, matsino2 = get_basismat_sinos(ct, sino1, sino2, spec1, spec2, n_iters=50)
+        matsino1.astype(np.float32).tofile(sub_dir+'mat1_sino_float32.bin')
+        matsino2.astype(np.float32).tofile(sub_dir+'mat2_sino_float32.bin')
+        if show_imgs:
+            fig, ax = plt.subplots(1, 2, dpi=300, figsize=[7,3])
+            ax_imshow(fig, ax[0], matsino1, title='Basis material 1')
+            ax_imshow(fig, ax[1], matsino2, title='Basis material 2')
             fig.tight_layout()
             plt.show()
-
-            ### 1b : back project
-            recon_HU = get_recon(out_dir, sino_log, ct, N_matrix, FOV, ramp, HU=True, spec=spec)
-            fig, ax = plt.subplots(1, 1, dpi=300, figsize=[6,5])
-            plt.imshow(recon_HU, cmap='gray')
-            plt.colorbar()
-            plt.show()
-        
-        ## 2 : Material decomposition (basis materials are hardcoded in matdecomp.py)
-        spec1 = Spectrum(filepath_spectrum+f'{spec_id1}_1mGy_float32.bin', spec_id1)
-        spec2 = Spectrum(filepath_spectrum+f'{spec_id2}_1mGy_float32.bin', spec_id2)
-        spec1.rescale_I0(ct.A_iso * D1 / ct.N_proj) 
-        spec2.rescale_I0(ct.A_iso * D2 / ct.N_proj) 
             
-        sino_aa = get_matdecomp(ct, phantom, spec1, spec2, D1, D2, N_matrix, FOV, ramp, n_iters=50)
-        fig, ax = plt.subplots(1, 2, dpi=300, figsize=[9,5])
-        m = ax[0].imshow(sino_aa[:,:,0], cmap='gray')
-        fig.colorbar(m, ax=ax[0])
-        m = ax[1].imshow(sino_aa[:,:,1], cmap='gray')
-        fig.colorbar(m, ax=ax[1])
-        fig.tight_layout()
-        plt.show()
+        # 2b : Reconstruct into basis material images.
+        if do_back_projection:
+            print('Back projecting basis material sinograms!')
+            recons = []
+            for i, matsino in enumerate([matsino1, matsino2]):
+                recon_raw, _ = get_recon(matsino, ct, spec1, N_matrix, FOV, ramp)  # spec is filler
+                recon_raw.astype(np.float32).tofile(sub_dir+f'mat{i+1}_recon_float32.bin')
+                recons.append(recon_raw)
+            if show_imgs:
+                fig, ax = plt.subplots(1, 2, figsize=[7,3])
+                ax_imshow(fig, ax[0], recons[0], title='Basis material 1')
+                ax_imshow(fig, ax[1], recons[1], title='Basis material 2')
+                fig.tight_layout()
+                plt.show()
 
         print(f'matdecomp finished for {spec_id1}-{spec_id2} : t={time() - t0:.2f}s') 
+
+
 
--- matdecomp.py
+++ matdecomp.py
@@ -17,12 +17,13 @@ matcomp2 = 'H(3.4)C(15.5)N(4.2)O(43.5)Na(0.1)Mg(0.2)P(10.3)S(0.3)Ca(22.5)'
 density2 = 1.92  # [g/cm3]
 
 
-def optimize_sino(Sino_gg, ee, i0, mus, n_iters, verbose=True):
+def optimize_sino(Sino_gg, ee, i0, mus, n_iters, verbose=True, dtype=cp.float32):
     """
     This function applies Newton Iterations to the sinogram to solve the
     material estimation problem. It is the first update in the ADMM scheme,
     where the primal variable is updated.
     
+    This uses cupy instead of numpy, but seems to give different results :-(
     Parameters
     ----------
     Sino_gg : (nMeas, nViews, nBins) numpy ndarray 
@@ -41,17 +42,17 @@ def optimize_sino(Sino_gg, ee, i0, mus, n_iters, verbose=True):
     Sino_aa : (nViews, nBins, nMats) numpy ndarray 
     """
     
-    Sino_gg = cp.array(Sino_gg, dtype=cp.float32)
-    ee = cp.array(ee, dtype=cp.float32)
-    i0 = cp.array(i0, dtype=cp.float32)
-    mus = cp.array(mus, dtype=cp.float32)
+    Sino_gg = cp.array(Sino_gg, dtype=dtype)
+    ee = cp.array(ee, dtype=dtype)
+    i0 = cp.array(i0, dtype=dtype)
+    mus = cp.array(mus, dtype=dtype)
 
     n_meas, nViews, nBins = Sino_gg.shape
     nMats = mus.shape[0]
 
     #[nViews, nBins, nMats]    
     EPS = 1e-6
-    Sino_aa = cp.full([nViews,nBins,nMats],EPS)   ## aa = density line integrals
+    Sino_aa = cp.full([nViews,nBins,nMats],EPS).astype(dtype)   ## aa = density line integrals
     
     #[nMeas, nMats, nBins, nEnergies]        
     ssff = i0[:,cp.newaxis,:,:]*mus[cp.newaxis,:,cp.newaxis,:]
@@ -82,8 +83,51 @@ def optimize_sino(Sino_gg, ee, i0, mus, n_iters, verbose=True):
 
     return Sino_aa.get()
 
+
+def optimize_sino_cpu(Sino_gg, ee, i0, mus, n_iters, verbose=True):
+    """
+    Copy of optimize_sino() but using numpy arrays (cpu) instead of cupy (gpu).
+    Cupy gives nonsense results, need to investigate why there is a difference.
+    Using cpu instead of gpu increases runtime ~ 8x but results in proper output. 
+    See optimize_sino.__doc__ for full documentation.
+    """
+    n_meas, nViews, nBins = Sino_gg.shape
+    nMats = mus.shape[0]
+
+    #[nViews, nBins, nMats]    
+    EPS = 1e-6
+    Sino_aa = np.full([nViews,nBins,nMats],EPS)   ## aa = density line integrals
+
+    #[nMeas, nMats, nBins, nEnergies]        
+    ssff = i0[:,np.newaxis,:,:]*mus[np.newaxis,:,np.newaxis,:]
+
+    #[nMeas, nMats, nMats, nBins, nEnergies]         
+    ssff2 = i0[:,np.newaxis,np.newaxis,:,:] * (mus[np.newaxis,:,:] * mus[:,np.newaxis,:])[np.newaxis,:,:,np.newaxis,:]
+
+    t0 = time.time()
+
+    for j in range(nViews):
+
+        if j%20==0 and verbose:
+            print(j, '/', nViews, f't={time.time() - t0:.2f}s')
+
+        for k in range(n_iters):
+
+            atten = np.exp(np.sum(-1.0*Sino_aa[j,:,:][:,:,np.newaxis]*mus, axis=1).clip(-700,700))
+
+            nu = np.sum(i0*atten,axis=2)
+            nu_grad = -1.0*np.sum(ssff*atten, axis=3)
+            nu_hess = np.sum(ssff2*atten, axis=4)
+
+            dF_poiss = -1.0*np.sum((Sino_gg[:,j,:]/nu-1.0)[:,np.newaxis,:]*nu_grad, axis=0)
+            H_poiss = -1.0*np.sum((Sino_gg[:,j,:]/nu-1.0)[:,np.newaxis,np.newaxis,:]*nu_hess - (Sino_gg[:,j,:]/(nu*nu))[:,np.newaxis,np.newaxis,:]*(nu_grad[:,np.newaxis,:,:]*nu_grad[:,:,np.newaxis,:]), axis=0)
+
+            Sino_aa[j,:,:] -= (np.linalg.inv(H_poiss.transpose([2,0,1])) * (dF_poiss.T)[:,np.newaxis,:]).sum(axis=2)
+
+    return Sino_aa
+
     
-def do_matdecomp_gn(sino1, sino2, spec1, spec2, id_phantom, ct, n_iters):
+def do_matdecomp_gn(ct, sino1, sino2, spec1, spec2, n_iters):
     
     N_proj, N_channels = sino1.shape
 
@@ -115,11 +159,53 @@ def do_matdecomp_gn(sino1, sino2, spec1, spec2, id_phantom, ct, n_iters):
         mus.append(u_E)
     mus = np.array(mus)
     
-    Sino_aa =  optimize_sino(Sino_gg, ee, i0, mus, n_iters)
+    #Sino_aa = optimize_sino(Sino_gg, ee, i0, mus, n_iters)
+    Sino_aa = optimize_sino_cpu(Sino_gg, ee, i0, mus, n_iters)
     return Sino_aa
     
 
-        
+def get_basismat_sinos(ct, sino_raw_1, sino_raw_2, spec1, spec2, n_iters=30, mask_thresh=0.95):
+    '''
+    For a DE-CT acquisition, make basis material sinograms using a
+    Gauss-Newton algorithm and reconstruct into basis material images.The 
+    basis materials are ICRU tissue (mat1) and bone (mat2). If desired, 
+    these can be changed in matdecomp.do_matdecomp_gn().
+    
+    Parameters
+    ----------
+    ct : ScannerGeometry
+        collection of parameters defining the CT acquisition geometry
+    sino_raw_1,2 : Spectrum
+        The two raw sinograms in units of photon counts.
+    spec1,2 : Spectrum
+        The two polychromatic x-ray spectra with counts scaled to the original dose.
+    n_iters : int, optional
+        Number of Gauss-Newton iterations for the mat decomp. The default is 30.
+    mask_thresh : float, 0 to 1, optional
+        Percent of maximum threshold for removing NaNs in sinos. The default is 0.95.
+
+    Returns
+    -------
+    sino_mat1, sino_mat2 : 2D numpy arrays
+        The two basis material sinograms.
+
+    '''
+
+    # make sino mask, to threshold out the outliers after mat decomp below
+    sino_mask = np.zeros(sino_raw_1.shape, dtype=bool)
+    sino_mask[sino_raw_1>=mask_thresh*np.max(sino_raw_1)] = 1
+ 
+    # call Gauss-Newton material decomposition algorithm
+    density_line_integrals = do_matdecomp_gn(ct, sino_raw_1, sino_raw_2, spec1, spec2, n_iters)
+    sino_mat1 = density_line_integrals[:,:,0]
+    sino_mat2 = density_line_integrals[:,:,1]
+    
+    # apply the mask
+    sino_mat1[sino_mask] = 0
+    sino_mat2[sino_mask] = 0
+
+    return sino_mat1, sino_mat2
+
 
 
 
